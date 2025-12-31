6fb23f1a5cc45a3b4ac7bdbdbc454f45ef7637f1
N60Top <N60GrowAgarden@gmail.com>
2025-12-31 05:36:02 +0300

    Update raw.js

--- api/raw.js
+++ api/raw.js
@@ -1,1 +1,11 @@
+export default function handler(req, res) {
+  const { code } = req.query;
 
+  if (!code) {
+    res.status(400).send("-- no script");
+    return;
+  }
+
+  res.setHeader("Content-Type", "text/plain");
+  res.send(decodeURIComponent(code));
+}
